f984e6e6facfa0d3cfaa76f990e60b544d3b7064
Juliana <juliana.lobato@yahoo.com.br>
2018-11-09 00:02:10 +0000

    maisUm

---
+++ src/main/java/com/javaee/springjpamysql/api/v1/model/CategoryDTO.java
@@ -0,0 +1,17 @@
+package com.javaee.springjpamysql.api.v1.model;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+
+	
+	private Long id;
+	
+	@NotBlank
+    private String description;
+}
